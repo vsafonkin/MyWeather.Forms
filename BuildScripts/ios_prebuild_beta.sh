ea9ccb0ce42cab6184bf0d2b522ddca5c3333b77
Yury Nevalenny <v-yuneva@microsoft.com>
2017-06-16 13:35:02 +0300

    add verification to script

--- BuildScripts/ios_prebuild_beta.sh
+++ BuildScripts/ios_prebuild_beta.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+echo "-== Started ios prebuild script ==-"
+
 app_name="Slate Pages Beta"
 bundle_id="com.piratespaceship.myweather"
 plist_path="Info.plist"
 
 # exit if a command fails
-set -e
+set -e || exit 1
 
 # verbose / debug print commands
-set -v
+set -v || exit 1
 
 # ---- Set Bundle ID:
-/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier ${bundle_id}" "${plist_path}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier ${bundle_id}" "${plist_path}" || exit 1
 
 # ---- Set App Name:
-/usr/libexec/PlistBuddy -c "Set :CFBundleName ${app_name}" "${plist_path}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleName ${app_name}" "${plist_path}" || exit 1
+
+echo "-== Finished ios prebuild script ==-"
