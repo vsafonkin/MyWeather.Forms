ebb045119ecda20c4f30245e4de373304e722905
yn <nevalenny@gmail.com>
2017-11-20 15:13:54 +0300

    update scripts

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,2 +1,8 @@
+echo "-- 1 --"
+find $APPCENTER_SOURCE_DIRECTORY -type d -name 'dSYMs' -print0
 iOSSymbolFileName="$(find $APPCENTER_SOURCE_DIRECTORY -type d -name 'dSYMs' -print0)"
+echo $iOSSymbolFileName
+echo "-- 2 --"
+find . -type d -name *.dSYM -print0
+iOSSymbolFileName="$(find . -type d -name *.dSYM -print0)"
 echo $iOSSymbolFileName
