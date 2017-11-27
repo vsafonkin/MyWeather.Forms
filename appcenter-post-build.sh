0119afd70a7d3e57f27f6e2d29c3c8f02427b84c
yn <nevalenny@gmail.com>
2017-11-27 21:38:41 +0300

    Add postbuild nunit tests run

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,8 +1,1 @@
-echo "-- 1 --"
-find $APPCENTER_SOURCE_DIRECTORY -type d -name 'dSYMs' -print0
-iOSSymbolFileName="$(find $APPCENTER_SOURCE_DIRECTORY -type d -name 'dSYMs' -print0)"
-echo $iOSSymbolFileName
-echo "-- 2 --"
-find . -type d -name *.dSYM -print0
-iOSSymbolFileName="$(find . -type d -name *.dSYM -print0)"
-echo $iOSSymbolFileName
+find . -name '*.nUnit.dll' -print0 | /usr/bin/xargs -0 -n1 nunit-console
