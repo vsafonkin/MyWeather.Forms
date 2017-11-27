541d6fbf6dcac30bad885af4658f9f5facd72b7b
yn <nevalenny@gmail.com>
2017-11-27 21:43:12 +0300

    Fix for unit tests

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,1 +1,2 @@
-find . -name '*.nUnit.dll' -print0 | /usr/bin/xargs -0 -n1 nunit-console
+find . -name '*.Tests.nUnit.dll' -print0 | /usr/bin/xargs -0 -n1 nunit-console
+find . -name 'TestResult.xml' -print0 | /usr/bin/xargs -0 -n1 cat
