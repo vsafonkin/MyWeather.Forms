55d9c00cd41f4eca601f2819e92b7a938fa082df
yn <nevalenny@gmail.com>
2017-11-27 21:49:33 +0300

    Find exec

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,2 +1,2 @@
-find . -name '*.Tests.nUnit.dll' -print0 | /usr/bin/xargs -0 -n1 nunit-console
-find . -name 'TestResult.xml' -print0 | /usr/bin/xargs -0 -n1 cat
+find . -name '*.Tests.nUnit.dll' -exec nunit-console {} \;
+find . -name 'TestResult.xml' -exec cat {} \;
